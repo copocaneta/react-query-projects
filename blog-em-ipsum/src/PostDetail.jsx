49697b897344210a042a4cba90d2803ecf65a351
Thiago Bernardi <copocaneta@gmail.com>
2023-03-05 21:46:16 -0300

    feat: implementing useMutation to delete and update posts

--- blog-em-ipsum/src/PostDetail.jsx
+++ blog-em-ipsum/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -32,6 +32,14 @@ export function PostDetail({ post }) {
     () => fetchComments(post.id)
   );
 
+  const deleteMutation = useMutation((postId) =>
+    deletePost(postId)
+  );
+
+  const updateMutation = useMutation((postId) =>
+    updatePost(postId)
+  );
+
   if (isLoading) return <h3>Loading...</h3>;
 
   if (isError)
@@ -45,7 +53,40 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button
+        onClick={() => deleteMutation.mutate(post.id)}
+      >
+        Delete
+      </button>
+      {deleteMutation.isError && (
+        <p style={{ color: 'red' }}>
+          Error deleting the post
+        </p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>
+          Post has been deleted
+        </p>
+      )}
+      <button
+        onClick={() => updateMutation.mutate(post.id)}
+      >
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>
+          Error updating the post
+        </p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
