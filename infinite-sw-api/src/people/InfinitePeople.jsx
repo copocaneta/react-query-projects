5d00c656b72b927d87dc60f48d43105bf4b3204b
Thiago Bernardi <copocaneta@gmail.com>
2023-03-12 21:03:30 -0300

    feat: [infinite-sw-api] implementing the useInfiniteQuery into the InfiniteScroll component

--- infinite-sw-api/src/people/InfinitePeople.jsx
+++ infinite-sw-api/src/people/InfinitePeople.jsx
@@ -9,15 +9,51 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  const { data, fetchNextPage, hasNextPage } =
-    useInfiniteQuery(
-      'sw-people',
-      ({ pageParam = initialUrl }) => fetchUrl(pageParam),
-      {
-        getNextPageParam: (lastPage) =>
-          lastPage.next || undefined
-      }
-    );
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error
+  } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.next || undefined
+    }
+  );
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+  if (isError) {
+    return <div className="error">Error: {error} </div>;
+  }
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <>
+      {isFetching && (
+        <div className="loading">Loading...</div>
+      )}
+      <InfiniteScroll
+        loadMore={fetchNextPage}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
